e1d721c7e7d7337a198434cd06d6db82b0acfcef
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-04-20 21:24:04 -0700

    todo-dom

--- Labs/Chatroom/server.js
+++ Labs/Chatroom/server.js
@@ -4,18 +4,84 @@ const portNumber = 8000;
 const file = require("./messages.json");
 const fs = require("fs");
 const url = require("url");
-
 const users = ["Steve", "Jack"];
 randomUser = function (users) {
   return users[Math.floor(Math.random() * users.length)];
 };
-
+const zeroes = (x) => {
+  return (x < 10 ? "0" : "") + x;
+};
+timestamp = function (time) {
+  let date = new Date(time);
+  let y = date.getFullYear();
+  let m = zeroes(date.getMonth() + 1);
+  let d = zeroes(date.getDate());
+  let h = zeroes(date.getHours());
+  let min = zeroes(date.getMinutes());
+  let result = `${y}-${m}-${d} ${h}:${min}`;
+  return result;
+};
+messageView = function (file) {
+  let username = file.username;
+  let timeposted = timestamp(file.timePosted);
+  let content = file.message;
+  let output = `
+${username}
+${timeposted}
+${content}
+    `;
+  return output;
+};
+messageList = function (file) {
+  for (newObj = [], i = 0; i < file.length; i++) {
+    newObj.push(messageView(file[i]));
+  }
+  return newObj;
+};
+htmlView = function (file) {
+  for (newObj = [], i = 0; i < file.length; i++) {
+    newObj.push(`<li> ${messageView(file[i])}</li>`);
+  }
+  return newObj;
+};
 const server = http.createServer((req, res) => {
   let newUrl = new URL(`http://localhost:${portNumber}${req.url}`);
 
   if (req.url === "/messages" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(file));
+    res.writeHead(200, { "Content-Type": "text/html" });
+    let results = JSON.stringify(messageList(file))
+      .split("\\n")
+      .join("\n")
+      .split('","')
+      .join("")
+      .split('["')
+      .join("")
+      .split('"]')
+      .join("");
+    let htmlCode = JSON.stringify(htmlView(file))
+      .split("\\n")
+      .join("<br>")
+      .split('","')
+      .join("")
+      .split('["')
+      .join("")
+      .split('"]')
+      .join("");
+    res.end(`
+    <!doctype html>
+    <html>
+    <body>
+        <div class = "messages">
+          ${htmlCode}
+        </div>
+        <form action="/messages" method="post">
+            <input type="text" name="username" /><br />
+            <input type="text" name="message" /><br />
+            <button>Save</button>
+        </form>
+    </body>
+    </html>
+`);
   } else if (req.url === "/messages" && req.method === "POST") {
     res.writeHead(200, { "Content-Type": "application/json" });
     const postDate = new Date().getTime();
@@ -41,29 +107,10 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.end("Message posted successfully");
       });
-
-      // fs.appendFile("./messages.json", message, function (err) {
-      //   if (err) {
-      //     console.log(err);
-      //   }
-      //   console.log(parse(body));
-      // });
     });
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ Message: "Route not found" }));
   }
 });
-
 server.listen(portNumber, () => console.log(`listening on port ${portNumber}`));
-
-//   Work done on 4/16
-
-//  -added res.on(end)
-//  -switched from appendFile to read file, have to read the file,
-//   parse it and push to get it to append to json array
-//  -imported query string to parse
-//  -formatted output message
-//  -successfully posting properly formatted json through curl and postman
-//  - added res.end to complete post request properly
-//  - tried to add functionality to choose a random user (not working properly yet)
